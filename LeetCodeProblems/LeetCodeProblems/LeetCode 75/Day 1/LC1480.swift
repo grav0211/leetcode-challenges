0923359961f30e2d26aeeb76099387c4e896fd0a
grav0211 <agrav065@gmail.com>
2022-11-21 18:27:57 -0500

    Add solutions and tests for LC1480

---
+++ LeetCodeProblems/LeetCodeProblems/LeetCode 75/Day 1/LC1480.swift
@@ -0,0 +1,52 @@
+//
+//  LC1480.swift
+//  LeetCodeProblems
+//
+//  Created by Alexandre Gravelle on 2022-11-21.
+//
+
+import Foundation
+
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+//
+// Return the running sum of nums.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+//
+// Example 2:
+//
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+//
+// Example 3:
+//
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+//
+//
+// Constraints:
+//
+// 1 <= nums.length <= 1000
+// -10^6 <= nums[i] <= 10^6
+
+public class LC1480 {
+    public init() {}
+    
+    public func runningSum(_ nums: [Int]) -> [Int] {
+        var totalSum = nums.reduce(0, +)
+        var runningSumArray = [Int]()
+        for i in stride(from: nums.count - 1, through: 0, by: -1) {
+            runningSumArray.insert(totalSum, at: 0)
+            totalSum -= nums[i]
+        }
+        
+        return runningSumArray
+    }
+}
--- LeetCodeProblems/LeetCodeProblemsTests/LeetCode 75/Day 1/LC1480Tests.swift
+++ LeetCodeProblems/LeetCodeProblemsTests/LeetCode 75/Day 1/LC1480Tests.swift
@@ -6,7 +6,28 @@
 //
 
 import XCTest
+import LeetCodeProblems
 
 class LC1480Tests: XCTestCase {
     
+    func testRunningSumOf1_2_3_4() {
+        let nums = [1,2,3,4]
+        
+        let result = LC1480().runningSum(nums)
+        XCTAssertEqual(result, [1,3,6,10])
+    }
+    
+    func testRunningSumOf1_1_1_1_1() {
+        let nums = [1,1,1,1,1]
+        
+        let result = LC1480().runningSum(nums)
+        XCTAssertEqual(result, [1,2,3,4,5])
+    }
+    
+    func testRunningSumOf3_1_2_10_1() {
+        let nums = [3,1,2,10,1]
+        
+        let result = LC1480().runningSum(nums)
+        XCTAssertEqual(result, [3,4,6,16,17])
+    }
 }

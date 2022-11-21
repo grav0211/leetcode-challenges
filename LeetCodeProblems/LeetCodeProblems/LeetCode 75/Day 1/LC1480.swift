304e4c0be4d5b67ff16b765b5548e74f81d6f834
grav0211 <agrav065@gmail.com>
2022-11-21 18:39:49 -0500

    Update test names in LC1480 to use exampleX instead of actual number array

--- LeetCodeProblems/LeetCodeProblems/LeetCode 75/Day 1/LC1480.swift
+++ LeetCodeProblems/LeetCodeProblems/LeetCode 75/Day 1/LC1480.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+// 1480. Running Sum of 1d Array
+//
 // Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 //
 // Return the running sum of nums.
--- LeetCodeProblems/LeetCodeProblemsTests/LeetCode 75/Day 1/LC1480Tests.swift
+++ LeetCodeProblems/LeetCodeProblemsTests/LeetCode 75/Day 1/LC1480Tests.swift
@@ -10,21 +10,21 @@ import LeetCodeProblems
 
 class LC1480Tests: XCTestCase {
     
-    func testRunningSumOf1_2_3_4() {
+    func testRunningSumOfExample1() {
         let nums = [1,2,3,4]
         
         let result = LC1480().runningSum(nums)
         XCTAssertEqual(result, [1,3,6,10])
     }
     
-    func testRunningSumOf1_1_1_1_1() {
+    func testRunningSumOfExample2() {
         let nums = [1,1,1,1,1]
         
         let result = LC1480().runningSum(nums)
         XCTAssertEqual(result, [1,2,3,4,5])
     }
     
-    func testRunningSumOf3_1_2_10_1() {
+    func testRunningSumOfExample3() {
         let nums = [3,1,2,10,1]
         
         let result = LC1480().runningSum(nums)

ee57d02cbb06e7f57efca0ef3533ae92647718ef
grav0211 <agrav065@gmail.com>
2022-11-21 18:38:50 -0500

    Add solution and tests for LC724

--- LeetCodeProblems/LeetCodeProblemsTests/LeetCode 75/Day 1/LC724Tests.swift
+++ LeetCodeProblems/LeetCodeProblemsTests/LeetCode 75/Day 1/LC724Tests.swift
@@ -6,7 +6,28 @@
 //
 
 import XCTest
+import LeetCodeProblems
 
 class LC724Tests: XCTestCase {
     
+    func testFindPivotIndexOfExample1() {
+        let nums = [1,7,3,6,5,6]
+        
+        let result = LC724().pivotIndex(nums)
+        XCTAssertEqual(result, 3)
+    }
+    
+    func testFindPivotIndexOfExample2() {
+        let nums = [1,2,3]
+        
+        let result = LC724().pivotIndex(nums)
+        XCTAssertEqual(result, -1)
+    }
+    
+    func testFindPivotIndexOfExample3() {
+        let nums = [2,1,-1]
+        
+        let result = LC724().pivotIndex(nums)
+        XCTAssertEqual(result, 0)
+    }
 }
